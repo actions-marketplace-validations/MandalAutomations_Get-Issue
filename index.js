7bec56a51aa77559b40a0392a99b79184b4b96d3
Evan Allen <eallen@xpirit.com>
2023-09-25 14:51:19 -0400

    add action.yml

---
+++ index.js
@@ -0,0 +1,24 @@
+import { context, getOctokit } from "@actions/github";
+
+const token = getInput("Token");
+const repoName = getInput("RepoName");
+const ownerName = getInput("OwnerName");
+const octokit = getOctokit(token);
+
+export const getAllIssues = async () => {
+    let url = `GET /repos/${ownerName}/${repoName}/issues`
+
+    return octokit.request(url, {
+        owner: ownerName,
+        repo: repoName,
+        labels: "response"
+    })
+}
+
+const run = async () => {
+    console.log("Running");
+    const issues = await getAllIssues();
+    console.log(issues);
+}
+
+run();
